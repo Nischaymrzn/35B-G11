34828415747712d596bd66f1caaf9cec30a4a8e7
Nischaymrzn <nischaymaharjann@gmail.com>
2024-07-29 18:42:08 +0545

    feat: bookingPage and Confirmed Page

--- src/eventhub/EventHub.java
+++ src/eventhub/EventHub.java
@@ -4,7 +4,7 @@
  */
 package eventhub;
 
-import eventhub.view.Login;
+import eventhub.view.user.ticketBookingPage;
 
 public class EventHub {
 
@@ -12,7 +12,7 @@ public class EventHub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Login ss= new Login();
+        ticketBookingPage ss= new ticketBookingPage();
         ss.setVisible(true);
     }
     
