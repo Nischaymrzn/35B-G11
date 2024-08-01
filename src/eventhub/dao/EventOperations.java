da27aa20a5b7e648581aa5bb7e99e8a25d12c7e5
CodeFFun <sharmasanketa899@gmail.com>
2024-08-01 13:31:40 +0545

    feat: Event Operations

--- src/eventhub/dao/EventOperations.java
+++ src/eventhub/dao/EventOperations.java
@@ -3,7 +3,6 @@ package eventhub.dao;
 import eventhub.database.DatabaseConnection;
 import eventhub.model.EventModel;
 import java.sql.*;
-import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -12,15 +11,13 @@ class EventOperations{
 
     public void addEvent(EventModel event){
         Connection conn = db.openConnection();
-        String sql = "INSERT INTO event (eventId, eventName, eventDescription, eventDate, eventTime, eventLocation, eventCategory) VALUES (?,?,?,?,?,?,?)";
+        String sql = "INSERT INTO event (eventId, eventName, eventDescription, eventDate, eventTime) VALUES (?,?,?,?,?,?,?)";
         try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pstmt.setInt(1, event.getEventId());
             pstmt.setString(2, event.getEventName());
             pstmt.setString(3, event.getEventDescription());
             pstmt.setString(4, event.getEventDate());
             pstmt.setString(5, event.getEventTime());
-            pstmt.setString(6, event.getEventLocation());
-            pstmt.setString(7, event.getEventCategory());
             pstmt.executeUpdate();
         } catch (SQLException ex) {
             Logger.getLogger(EventOperations.class.getName()).log(Level.SEVERE, null, ex);
